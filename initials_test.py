ba5315b3069030a4f9df8be05589dc51d5d0a070
Augustus Buckman <abuckman@gctu.edu.gh>
2023-08-01 16:41:22 +0000

    Create initials_test.py

---
+++ initials_test.py
@@ -0,0 +1,15 @@
+import os.path
+import sys
+from initials import main
+
+def initials_test():
+  try:
+    exists=os.path.exists("initials.py")
+    assert exists == true
+  except:
+    sys.exit()
+main()
+output = get_display_output()
+#assert output == [
+ # ""
+#]
