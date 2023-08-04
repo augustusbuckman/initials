b2fecc71dbb8f917cf9d4a4231354390ad70bbe8
Augustus Buckman <b020112032@live.gtuc.edu.gh>
2023-08-04 18:41:54 +0000

    Initials Test file corrected

--- initials_test.py
+++ initials_test.py
@@ -2,13 +2,12 @@ import os.path
 import sys
 from initials import main
 
-def initials_test():
-  try:
-    exists=os.path.exists("initials.py")
-    assert exists == True
-  except:
-    sys.exit()
 
-  
-  main()
+def test_initials():
+    try:
+        exists = os.path.exists("initials.py")
+        assert exists == True
+    except:
+        sys.exit()
 
+    main()
